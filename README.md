c95c408269daf47c2f91d01bb55174536fb3aa05
Maurizio Cingolani <mauriziocingolani74@gmail.com>
2015-12-17 12:21:22 +0100

    Inizializzazione componente

---
+++ README.md
@@ -0,0 +1,9 @@
+# yii2-fmwk-telegrambot
+
+Estensione per interagire con il Bot Telegram relativo all'applicazione.
+
+In fase di configurazione richiede i seguenti parametri (file di configurazione telegrambot.php):
+
+...
+
+Licenza:  [BSD-3-Clause](http://opensource.org/licenses/BSD-3-Clause)
---
+++ TelegramBot.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace mauriziocingolani\yii2fmwktelegrambot;
+
+use yii\base\InvalidConfigException;
+
+/**
+ * Componente per l'interazione con il Telegram Bot relativo all'applicazione.
+ * Richiede l'inizializzazione in fase di configurazione dei seguenti parametri:
+ * <ul>
+ * <li>$token: Token del Bot</li>
+ * </ul>
+ * @author Maurizio Cingolani <mauriziocingolani74@gmail.com>
+ * @author Maurizio Cingolani <mauriziocingolani74@gmail.com>
+ * @license http://opensource.org/licenses/BSD-3-Clause BSD-3-Clause
+ * @version 1.0
+ */
+class Twitter extends \yii\base\Component {
+
+    public $token;
+
+    /**
+     * Inizializza il componente e verifica che i parametri siano stati impostati.
+     * @throws InvalidConfigException
+     */
+    public function init() {
+        parent::init();
+        if (!$this->token)
+            throw new InvalidConfigException('Componente Telegram Bot: parametri di configurazione mancanti.');
+    }
+
+}
