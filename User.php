ca123e308c342f964e8bfc7843f301e9e82666a9
Maurizio Cingolani <mauriziocingolani74@gmail.com>
2017-08-24 08:34:24 +0200

    Baco assegnazione proprietà a User

--- User.php
+++ User.php
@@ -12,7 +12,7 @@ namespace mauriziocingolani\yii2fmwktelegrambot;
  * 
  * @author Maurizio Cingolani <mauriziocingolani74@gmail.com>
  * @license http://opensource.org/licenses/BSD-3-Clause BSD-3-Clause
- * @version 1.0.1
+ * @version 1.0.2
  * 
  * @see https://core.telegram.org/bots/api#user
  */
@@ -41,7 +41,8 @@ class User extends \yii\base\Object {
     public function __construct($object) {
         foreach ($object as $key => $value) :
             $k = "_$key";
-            $this->$k = $value;
+            if (property_exists($this, $k))
+                $this->$k = $value;
         endforeach;
     }
 
